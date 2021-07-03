2a08ab81751c577ee75b33e5a6118f2edd232f37
Mjgiannelli <51857101+mjgiannelli@users.noreply.github.com>
2021-07-03 14:17:43 -0500

    create pseudo code for employee test

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,10 @@
+// we will create a mock employee
+// we need to test name tobe the name we pass through
+// we need to test that the ID is equal to any number
+// we need to test that email is equal to the email passed through and string contains an'@' symbol
+// we need to test that getName has the property of name
+// we need to test that getEmail has the property of email
+// we need to test that getID has the property of ID
+// we need to test that getRole has the property of employee
+
+
