30aba2581f48878edbca645db6493d55ed6fa0f6
Mjgiannelli <51857101+mjgiannelli@users.noreply.github.com>
2021-07-03 22:04:36 -0500

    add initial html page generation template

--- src/html.js
+++
@@ -1,9 +0,0 @@
-// this is where we will create functions to write the html template and pass in the responses from the prompts
-// include a header
-// container
-// one row
-// add a card anytime a new employee is added
-// card contains a container
-// 2 rows
-// first row two columns (Name and icon)
-// second row 3 columns (list)
---
+++ src/page-template.js
@@ -0,0 +1,43 @@
+// this is where we will create functions to write the html template and pass in the responses from the prompts
+// include a header
+// container
+// one row
+// add a card anytime a new employee is added
+// card contains a container
+// 2 rows
+// first row two columns (Name and icon)
+// second row 3 columns (list)
+
+// const generateEmployees = employeesArray => {
+//     return `
+//     <section class='row-cards'>
+//         <div class='col'>
+//         ${employeesArray
+//         .map(({ name}))}    
+//     `;
+// }
+
+module.exports = templateData => {
+    return `
+  <!DOCTYPE html>
+  <html lang="en">
+
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Organziation</title>
+    <link rel="stylesheet" href="style.css">
+  </head>
+
+  <body>
+    <header>
+      <h1>My Team<h1>
+    </header>
+    <main class="container">
+        <h2>${templateData}</h2>
+    </main>
+  </body>
+  </html>
+  `;
+};
